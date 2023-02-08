6a896c9707d3c0d6f27b7d978c2504ea2082cab3
chidiebereibiam <chidiebereorjiibiam7@gmail.com>
2023-02-08 19:22:42 +0100

    Finished Frontend Design of Register Page

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
+from django.contrib.auth.models import User
+
 # Create your models here.
+class Profile(models.Model):
+    GENDER_CHOICES = (
+        ('Male', 'Male'),
+        ('Female', 'Female'),
+        ('Others Specify', 'Others Specify')
+    )
+
+    user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255, null = True)
+    about = models.TextField(null=True)
+    date_of_birth = models.DateField(null = True)
+    gender = models.CharField(max_length=50, null = True, choices = GENDER_CHOICES)
+    linkedin_id = models.CharField(max_length=255, null=True, blank = True)
+    facebook_handle = models.CharField(max_length=255, null=True, blank = True)
+    twitter_handle = models.CharField(max_length=255, null=True, blank = True)
+    instagram_handle = models.CharField(max_length=255, null=True, blank = True)
+    website = models.CharField(max_length=255, null=True, blank = True)
+    country = models.CharField(max_length=255, null=True, blank = True)
+    state = models.CharField(max_length=255, null=True, blank = True)
+    profile_pic = models.ImageField(null=True, blank = True, upload_to = "profile/")
+
+    def __str__(self):
+        return self.name
+    
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import SignUpForm
+from django.contrib.auth.models import User
+from django.urls import reverse_lazy, reverse
+from django.contrib.auth.models import Group
+
 
 # Create your views here.
+def register(request):
+    form = SignUpForm()
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            username = form.cleaned_data.get('username')
+            return redirect('login')
+    context = {'form':form}
+    return render(request, 'registration/register.html', context)

96c93b19b58cca712eba8b8cdb5d8f0702099c6e
chidiebereibiam <chidiebereorjiibiam7@gmail.com>
2023-02-08 20:42:55 +0100

    Completed User Login

--- certificate/settings.py
+++ certificate/settings.py
@@ -134,6 +134,6 @@ STATICFILES_DIRS = [
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-LOGIN_URL = '/accounts/login'
+LOGIN_URL = '/accounts/login/'
 LOGIN_REDIRECT_URL = 'dashboard'
-LOGOUT_REDIRECT_URL = '/accounts/login'
+LOGOUT_REDIRECT_URL = '/accounts/login/'

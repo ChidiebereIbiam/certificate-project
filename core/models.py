a4bb698f56a585e3791a822669a8f61e9527f2f2
Chidiebere Ibiam <chidiebereorjibiam7@gmail.com>
2023-02-15 06:02:20 -0800

    Completed Certificate Generation with QR Code

--- core/models.py
+++ core/models.py
@@ -1,3 +1,4 @@
+import random
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
--- core/views.py
+++ core/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 from .forms import CertificateForm
 from .models import Certificate
 
@@ -8,10 +10,24 @@ from .models import Certificate
 def dashboard(request):
     return render(request, 'core/dashboard.html', {})
 
-def certificate_detail(request):
-    return render (request, 'core/certificate_detail.html', {})
+def certificate_detail(request, id):
+    cert_details = Certificate.objects.get(id=id)
+    context = {'cert_details':cert_details}
+    return render (request, 'core/certificate_detail.html', context)
 
 def generate_certificate(request):
     if request.method == 'POST':
+        profile_photo = request.FILES['profile_photo']
         cerificate_number = request.POST['certificate_number']
+        name = request.POST['c_name']
+        role = request.POST['role']
+        organization = request.POST['organization']
+        organizer = request.POST['organizer']
+        start_date = request.POST['start_date']
+        end_date = request.POST['end_date']
+        verification_link = f"http://127.0.0.1:8000/{cerificate_number}/verify"
+        Certificate.objects.create(profile_photo=profile_photo, certificate_number = cerificate_number, name = name, role=role, organization=organization, organizer=organizer, start_date=start_date, end_date=end_date, verification_link=verification_link)
+        
+        return HttpResponseRedirect(reverse('dashboard'))
+
     return render(request, 'core/generate_certificate.html')
